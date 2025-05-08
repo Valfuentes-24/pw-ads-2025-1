3af62abf31b12e295704c1ef8c49a51a563d7bc4
Valfuentes-24 <valquiriafuentes@hotmail.com>
2025-05-08 12:28:41 -0300

    (08/05) Criação do projeto Karangos

---
+++ karangos/src/App.jsx
@@ -0,0 +1,20 @@
+//import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import TopBar from './UI/TopBar';
+import CssBaseline  from '@mui/material/CssBaseline';
+
+
+function App() {
+  
+  return (
+    <>
+      <CssBaseline/>
+      <TopBar/>
+    </>
+  )
+}
+
+export default App
