b4a7a3ad98bb46e48597c5a8099b5743b5141ae9
Valfuentes-24 <valquiriafuentes@hotmail.com>
2025-05-22 12:31:41 -0300

    (22/05) Habilitados navegação e modo escuro

--- karangos/src/ui/TopBar.jsx
+++ karangos/src/ui/TopBar.jsx
@@ -8,7 +8,7 @@ import MainMenu from './MainMenu';
 export default function TopBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" enableColorOnDark>
         <Toolbar variant="dense">
           
           <MainMenu />
