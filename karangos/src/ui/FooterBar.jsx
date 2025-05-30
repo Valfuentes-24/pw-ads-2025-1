fa4e9f4baac82cf560c0267696da82415cd4484e
Valfuentes-24 <valquiriafuentes@hotmail.com>
2025-05-30 09:18:12 -0300

    (30/05) Término da listagem de clientes e início do cadastro

--- karangos/src/ui/FooterBar.jsx
+++ karangos/src/ui/FooterBar.jsx
@@ -1,25 +1,32 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Typography from '@mui/material/Typography'
 import LocalCafeIcon from '@mui/icons-material/LocalCafe';
 
 export default function FooterBar() {
   return (
     <Box 
-        component="footer" 
-        sx={{ 
-            backgroundColor: 'action.disabledBackground',
-            display: 'flex',
-            justifyContent: 'center',
-            position: 'fixed', //posição fixa
-            bottom: 0, //na parte debaixo da página
-            width: '100%', //100% da largura do viewport
-    }}
+      component="footer" 
+      sx={{ 
+        backgroundColor: 'action.disabledBackground',
+        display: 'flex',
+        justifyContent: 'center',
+        position: 'fixed',  // posição fixa
+        bottom: 0,          // na parte de baixo da página
+        width: '100vw'      // 100% da largura da viewport
+      }}
     >
-    <Typography variant="caption" gutterBottom> 
-      Desenvolvido com <LocalCafeIcon fontSize='small'/> por <a
-      href="valvolfu@gmail.com"> Valquiria</a>
-    </Typography>
+      <Typography 
+        variant="caption" 
+        gutterBottom
+        sx={{
+          '& a': {  // Altera a cor do link (a) dentro do Typography
+            color: 'secondary.light'
+          }
+        }}
+      >
+        Desenvolvido e mantido com <LocalCafeIcon fontSize="small" /> por <a href="mailto:professor@faustocintra.com.br">Prof. Fausto Cintra</a>
+      </Typography>
     </Box>
   );
 }
